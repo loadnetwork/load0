018a8c7eb7baa1b5d1a826b8791468cf70f71460
the-db-architect <the-db-architect@users.noreply.github.com>
2025-08-12 20:34:25 -0400

    chore: Change auth endpoint

--- src/utils/auth.rs
+++ src/utils/auth.rs
@@ -1,4 +1,4 @@
-pub const AUTH_API_URL: &str = "https://ipfs.rs";
+pub const AUTH_API_URL: &str = "https://k8s.load-auth-service.load.network";
 
 pub fn is_access_token_valid(token: &str) -> bool {
     let req = ureq::get(format!("{}/internal/verify/{}", AUTH_API_URL, token)).call();
